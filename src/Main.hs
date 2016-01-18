153b5be260bf8d222b14d9d8ce4d9408150be0bc
Artyom <yom@artyom.me>
2016-01-18 15:28:07 +0300

    Reorder things around, use Megaparsec

--- src/Main.hs
+++ src/Main.hs
@@ -19,6 +19,10 @@ import Control.Monad.Except
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+-- Parsing
+import Text.Megaparsec
+import Text.Megaparsec.Text
+import Text.Megaparsec.Lexer (integer)
 -- Concurrency and MVars
 import Control.Concurrent.Lifted
 -- Exceptions
@@ -31,44 +35,78 @@ import Telegram
 
 main :: IO ()
 main = void $ do
-  token <- T.readFile "telegram-token"
-  runTelegram token bot
+  botToken <- T.readFile "telegram-token"
+  runTelegram botToken bot
 
-ignoreErrors :: Telegram a -> Telegram ()
-ignoreErrors x = void x `catchError` (liftIO . print)
+bot :: Telegram ()
+bot = do
+  -- Create a variable holding all reminders.
+  remindersVar <- newMVar []
+  let schedule :: Reminder -> Telegram ()
+      schedule r = modifyMVar_ remindersVar (return . (r:))
+  -- Run the loop that checks all reminders every second, sends messages
+  -- about ones that have fired, and leaves those that haven't fired yet.
+  fork $ forever $ ignoreErrors $ do
+    modifyMVar_ remindersVar $ \reminders -> do
+      (fired, later) <- liftIO $ findDueReminders reminders
+      for_ fired $ \Reminder{..} -> sendMessage chatId msg
+      return later
+    threadDelay 1000000
+  -- Run the loop that accepts incoming messages.
+  onUpdateLoop $ \Update{..} -> ignoreErrors $ ignoreExceptions $ do
+    case text message of
+      Nothing ->
+        return ()
+      Just str -> void $ do
+        case parseReminder str of
+          Nothing -> respond message "couldn't parse what you said"
+          Just (seconds, msg) -> do
+            currentTime <- liftIO $ getCurrentTime
+            let time = addUTCTime (fromIntegral seconds) currentTime
+            schedule Reminder {
+              time   = time,
+              chatId = chat_id (chat message),
+              msg    = msg }
+            respond message ("scheduled at " <> T.pack (show time))
 
-ignoreExceptions :: Telegram a -> Telegram ()
-ignoreExceptions x = void x `catch` \(SomeException a) -> liftIO (print a)
+-- Reminders
 
 data Reminder = Reminder {
   time   :: UTCTime,
   chatId :: Integer,
   msg    :: Text }
 
-bot :: Telegram ()
-bot = do
-  remindersVar <- newMVar []
-  fork $ forever $ do
-    modifyMVar_ remindersVar $ \reminders -> do
-      currentTime <- liftIO $ getCurrentTime
-      let (fired, later) = partition ((< currentTime) . time) reminders
-      for_ fired $ \Reminder{..} -> ignoreErrors $
-        sendMessage chatId msg
-      return later
-    threadDelay 1000000
-  let schedule :: Reminder -> Telegram ()
-      schedule r = modifyMVar_ remindersVar (return . (r:))
-  onUpdateLoop $ \Update{..} -> ignoreErrors $ ignoreExceptions $ do
-    case text message of
-      Nothing ->
-        return ()
-      Just str -> void $ do
-        let (d, rest) = T.break (== ' ') str
-        currentTime <- liftIO $ getCurrentTime
-        let seconds = read (T.unpack d) :: Integer
-            reminderTime = addUTCTime (fromIntegral seconds) currentTime
-        schedule Reminder {
-          time   = reminderTime,
-          chatId = chat_id (chat message),
-          msg    = T.tail rest }
-        respond message ("scheduled at " <> T.pack (show reminderTime))
+findDueReminders :: [Reminder] -> IO ([Reminder], [Reminder])
+findDueReminders rs = do
+  currentTime <- getCurrentTime
+  return (partition ((< currentTime) . time) rs)
+
+-- Parsing
+
+parseReminder :: Text -> Maybe (Integer, Text)
+parseReminder = parseMaybe reminderP
+
+reminderP :: Parser (Integer, Text)
+reminderP = do
+  duration <- durationP
+  skipSome spaceChar
+  rest <- many anyChar
+  return (duration, T.pack rest)
+
+durationP :: Parser Integer
+durationP = do
+  items <- some $ do
+    n <- integer
+    choice [
+      string "h" *> pure (n*3600),
+      string "m" *> pure (n*60),
+      string "s" *> pure n ]
+  return (sum items)
+
+-- Utils
+
+ignoreErrors :: Telegram a -> Telegram ()
+ignoreErrors x = void x `catchError` (liftIO . print)
+
+ignoreExceptions :: Telegram a -> Telegram ()
+ignoreExceptions x = void x `catch` \(SomeException a) -> liftIO (print a)

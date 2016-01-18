58063d2ac20280c0d4251de34f660f8cc49d7120
Artyom <yom@artyom.me>
2016-01-18 20:10:29 +0300

    The status command

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE
 RecordWildCards,
 OverloadedStrings,
+MultiWayIf,
 NoImplicitPrelude
   #-}
 
@@ -19,6 +20,10 @@ import Control.Monad.Except
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy as TL
+import Data.Text.Format hiding (format, print)
+import qualified Data.Text.Format as Format (format)
+import Data.Text.Format.Params (Params)
 -- Parsing
 import Text.Megaparsec hiding (Message)
 import Text.Megaparsec.Text
@@ -57,27 +62,43 @@ bot = do
     case text message of
       Nothing ->
         return ()
+      Just "?" -> void $ do
+        reminders <- readMVar remindersVar
+        respond message =<< liftIO (showStatus reminders)
       Just str -> void $ do
         case parseReminder str of
           Nothing -> respond message "couldn't parse what you said"
-          Just (seconds, _) -> do
+          Just (seconds, reminderText) -> do
             currentTime <- liftIO $ getCurrentTime
             let time = addUTCTime (fromIntegral seconds) currentTime
             schedule Reminder {
               time            = time,
-              originalMessage = message }
-            respond message ("scheduled at " <> T.pack (show time))
+              originalMessage = message,
+              reminderText    = reminderText }
+            respond message (format "scheduled at {}" [time])
 
 -- Reminders
 
 data Reminder = Reminder {
   time            :: UTCTime,
-  originalMessage :: Message }
+  originalMessage :: Message,
+  reminderText    :: Text }
 
 findDueReminders :: [Reminder] -> IO ([Reminder], [Reminder])
 findDueReminders rs = do
   currentTime <- getCurrentTime
   return (partition ((< currentTime) . time) rs)
+
+showStatus :: [Reminder] -> IO Text
+showStatus rs = do
+  currentTime <- getCurrentTime
+  let showLeft r = showDuration (diffUTCTime (time r) currentTime) <> " left"
+  return $ case rs of
+    []  -> "you're not doing anything"
+    [r] -> format "{} – {}" (reminderText r, showLeft r)
+    _   -> T.unlines $ do
+             (i, r) <- zip [1 :: Int ..] rs
+             return (format "{}. {} – {}" (i, reminderText r, showLeft r))
 
 -- Parsing
 
@@ -109,3 +130,31 @@ ignoreErrors x = void x `catchError` (liftIO . print)
 
 ignoreExceptions :: Telegram a -> Telegram ()
 ignoreExceptions x = void x `catch` \(SomeException a) -> liftIO (print a)
+
+showDuration :: NominalDiffTime -> Text
+showDuration diff = do
+  let (h, rest) = (ceiling diff :: Integer) `divMod` 3600
+      (m, s)    = rest `divMod` 60
+  if | h == 0 && m == 0 && s == 0 -> "nothing"
+     | h == 0 && m < 5 ->
+         -- being precise
+         T.unwords $ [showPlural m "minute" | m /= 0] ++
+                     [showPlural s "second" | s /= 0]
+     | otherwise ->
+         -- rounding
+         let m_all :: Double
+             m_all = fromIntegral h * 60
+                   + fromIntegral m
+                   + fromIntegral s / 60
+             (h', m') = round m_all `divMod` 60
+         in T.unwords $ [showPlural h' "hour"   | h' /= 0] ++
+                        [showPlural m' "minute" | m' /= 0]
+
+showPlural :: Integer -> Text -> Text
+showPlural n x
+  | n `mod` 10 == 1 && n `mod` 100 /= 11 = T.pack (show n) <> " " <> x
+  | otherwise                            = T.pack (show n) <> " " <> x <> "s"
+
+-- | A wrapper around 'Format.format' that returns a strict Text.
+format :: Params ps => Format -> ps -> Text
+format f ps = TL.toStrict (Format.format f ps)
